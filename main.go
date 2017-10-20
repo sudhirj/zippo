7e96743a1a920c466d7b3b5074cf533bc1129363
Sudhir Jonathan <sudhir.j@gmail.com>
2017-10-20 16:14:10 +0530

    Parallelized downloads correctly while keeping the zipping serial

--- main.go
+++ main.go
@@ -8,10 +8,18 @@ import (
 	"os"
 	"time"
 
+	"sync"
+
+	"strconv"
+
 	_ "github.com/heroku/x/hmetrics"
 )
 
-type remote_file struct {
+type fileDef struct {
+	path string
+	url  string
+}
+type remoteFile struct {
 	path     string
 	response *http.Response
 }
@@ -33,12 +41,15 @@ func main() {
 
 		archive := zip.NewWriter(w)
 
-		receiver := make(chan remote_file, 10)
-		done := make(chan error)
+		downloadQueue := make(chan fileDef)
+		archiveQueue := make(chan remoteFile)
+		downloaderWaitGroup := sync.WaitGroup{}
+		finished := make(chan error)
 
+		// Run archiver
 		go func() {
-			for rf := range receiver {
-				log.Println("Received response for " + rf.path)
+			for rf := range archiveQueue {
+				log.Println("Received data for " + rf.path)
 				entryHeader := &zip.FileHeader{
 					Name:   rf.path,
 					Method: zip.Deflate,
@@ -47,46 +58,61 @@ func main() {
 				entryHeader.SetMode(os.ModePerm)
 				entry, err := archive.CreateHeader(entryHeader)
 				if err != nil {
-					done <- err
+					finished <- err
 					return
 				}
-
+				log.Println(rf.response.Header.Get("Last-Modified"))
 				modifiedTime, err := time.Parse("Mon, 2 Jan 2006 15:04:05 MST", rf.response.Header.Get("Last-Modified"))
 				if err != nil {
 					entryHeader.SetModTime(modifiedTime)
 				}
+
 				io.Copy(entry, rf.response.Body)
 				rf.response.Body.Close()
 				log.Println("Zipped and closed " + rf.path)
 			}
-			log.Println("All zipping complete, finishing up.")
-			done <- nil
-		}()
-
-		for path, urls := range r.PostForm {
-			log.Println("Downloading key: " + path + " URL: " + urls[0])
-			download, err := http.Get(urls[0])
+			log.Println("All zipping complete, closing up.")
+			err := archive.Close()
 			if err != nil {
-				handleError(err, w)
+				finished <- err
 				return
 			}
-			receiver <- remote_file{path: path, response: download}
-		}
-		log.Println("All downloads finished, closing receiver.")
-		close(receiver)
+			log.Println("Acrhive closed.")
+			finished <- nil
+		}()
 
-		err := <-done
-		if err != nil {
-			handleError(err, w)
-			return
+		// Run downloaders
+		for w := 1; w <= 5; w++ {
+			go func(w int) {
+				log.Println("Downloader Ready: " + strconv.Itoa(w))
+				for df := range downloadQueue {
+					log.Println("Downloading[" + strconv.Itoa(w) + "] path: " + df.path + " URL: " + df.url)
+					download, err := http.Get(df.url)
+					if err != nil {
+						downloaderWaitGroup.Done()
+						finished <- err
+						return
+					}
+					archiveQueue <- remoteFile{path: df.path, response: download}
+					downloaderWaitGroup.Done()
+				}
+				log.Println("Downloader Shutting Down: " + strconv.Itoa(w))
+			}(w)
 		}
-		err = archive.Close()
+
+		for path, urls := range r.PostForm {
+			downloaderWaitGroup.Add(1)
+			downloadQueue <- fileDef{path: path, url: urls[0]}
+		}
+		close(downloadQueue)
+		downloaderWaitGroup.Wait()
+		close(archiveQueue)
+		err := <-finished
 		if err != nil {
 			handleError(err, w)
 			return
 		}
 	}))
-
 }
 
 func handleError(err error, w http.ResponseWriter) {
